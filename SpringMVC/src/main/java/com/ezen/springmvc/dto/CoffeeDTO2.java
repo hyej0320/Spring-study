e092d25a9ea4c1c60e78febbd286c9ab74dd3b7c
Hyejin <hyej0320@gmail.com>
2024-08-07 23:29:47 +0900

    first commit

---
+++ SpringMVC/src/main/java/com/ezen/springmvc/dto/CoffeeDTO2.java
@@ -0,0 +1,10 @@
+package com.ezen.springmvc.dto;
+
+import lombok.Data;
+
+@Data
+public class CoffeeDTO2 {
+	private String coffee_kor_name;
+	private Integer coffee_price;
+	private String coffee_eng_name;
+}
