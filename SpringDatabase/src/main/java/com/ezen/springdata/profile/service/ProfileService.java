96ca800ef01c8d40c1c43f290e86cb42a815f799
Hyejin <hyej0320@gmail.com>
2024-08-14 11:36:33 +0900

    수업내용 update

--- SpringDatabase/src/main/java/com/ezen/springdata/profile/service/ProfileService.java
+++ SpringDatabase/src/main/java/com/ezen/springdata/profile/service/ProfileService.java
@@ -2,6 +2,7 @@ package com.ezen.springdata.profile.service;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -22,23 +23,44 @@ public class ProfileService {
 	public void save(ProfileImageDTO profileImageDTO) throws IllegalStateException, IOException {
 		// 1. 클라이언트로부터 전달 받은 파일을 서버 컴퓨터에 내보내기
 		MultipartFile profileImageFile = profileImageDTO.getProfileImageFile();
+//		
+//		if (profileImageFile == null) {
+//			return;
+//		}
+
+		saveFile(profileImageFile);
+	}
+	
+	public void multisave(ProfileImageDTO profileImageDTO) throws IllegalStateException, IOException {
+		List<MultipartFile> profileImageFiles = profileImageDTO.getProfileImageFiles();
+//		
+//		if (profileImageFiles == null || profileImageFiles.size() == 0) {
+//			return ;
+//		}
+		for (MultipartFile profileImageFile : profileImageFiles) {
+			saveFile(profileImageFile);
+		}
+	}
+
+	String rootPath = "C:/spring_upload_files/";
+	
+	public void saveFile(MultipartFile profileImageFile) throws IllegalStateException, IOException {
+		
+		if (profileImageFile == null || profileImageFile.isEmpty()) {
+			return ;
+		}
 		
 		String originalFileName = profileImageFile.getOriginalFilename();
 		String storedFileName = String.format("%s-%s", System.currentTimeMillis(), originalFileName);
-		
-		log.info("#### 내보내야 하는 첨부 파일 정보 ####");
-		log.info("profileImageFile: {}", profileImageFile);
-		log.info("originalFileName: {}", originalFileName);
-		log.info("storedFileName: {}", storedFileName);
-		
-		String savePath = "C:/spring_upload_files/" + storedFileName;
+		String savePath = rootPath + storedFileName;
 		
 		profileImageFile.transferTo(new File(savePath));
 		
-		// 2. 서버 컴퓨터에 파일 쓰기를 성공했다면 DB에 내용을 반영한다
+		ProfileImageDTO profileImageDTO = new ProfileImageDTO();
+		// 2. 서버 컴퓨터에 파일 쓰기를 성공했다면 DB에 경로들을 보관
 		profileImageDTO.setOriginalFileName(originalFileName);
 		profileImageDTO.setStoredFileName(storedFileName);
-		
+
 		profileRepository.saveProfileImage(profileImageDTO);
 	}
 }
